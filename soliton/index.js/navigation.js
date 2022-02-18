8f62ceac4a901de31d38f2f5606539e1d499dbe6
ayazibrahimov <ayaz.ibrahimov.1998@mail.ru>
2022-02-18 12:32:26 +0400

    soliton

--- soliton/index.js/navigation.js
+++ soliton/index.js/navigation.js
@@ -4,16 +4,29 @@ const newSlide = ()=>{
     const burger =document.querySelector(".burger")
     const navmenu = document.querySelector(".navigation-menu")
     const navLists = document.querySelectorAll("#nav-list li")
-
+    const body = document.querySelector("body")
+    const show = document.querySelector(".show")
+    const backdark = document.querySelector(".backdark")
     
     burger.addEventListener('click',()=>{
          navmenu.classList.toggle("toggle-menu")
-    
+         
+
          navLists.forEach((navList,index)=>{
             if(navList.style.animation){
                 navList.style.animation = ''
+                backdark.style.opacity=''
+                backdark.style.background=''
+                body.style.overflow=''
+                 
               }else{
                 navList.style.animation=`fadeOut .2s ease forwards ${index / 15 -0.2 }s` 
+                backdark.style.opacity='.1'
+                
+                body.style.overflow='hidden'
+                
+                 
+                
               }
               
          })
@@ -30,3 +43,6 @@ const newSlide = ()=>{
 
 
 newSlide()
+
+
+
--- soliton/index.js/slide.js
+++ soliton/index.js/slide.js
@@ -1,26 +1,102 @@
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
+
+
+// en usdeki slider
+let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 0,
     centeredSlides: true,
     autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+      delay: 2000,
+      disableOnInteraction: false,
     },
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+      el: ".swiper-pagination",
+      clickable: true,
     },
     navigation: {
-        nextEl: ".swiper-button-1",
-        prevEl: ".swiper-button-2",
-    }
-});
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
+  // marka slideri
+  //2-ci slider
+
+  $('.marks').slick({
+    dots: false,
+    speed: 400,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed:1800,
+  
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+     
+    ]
+  });
+
+
+
+
+
+
+//servise slideri 
+// 3 ve 4 slider
 
 $('.service-slider-1').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
+    
   });
           
 $('.service-slider-2').slick({
@@ -28,7 +104,87 @@ $('.service-slider-2').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
   });
           
 
+$('.slider--fixing__carusel').slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1800,
+  pauseOnFocus:false,
+  speed:400,
+  
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 1270,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+ 
+  ]
+});
+			
 
+
+
+
+
+
+// SCROLL
